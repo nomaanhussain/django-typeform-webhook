7c91a68d59d0b962206eefb6370a899fa8f52c16
nomaanhussain <nomanghanchi_11@yahoo.com>
2022-03-02 23:47:10 +0500

    making webhook secure

--- webhook/views.py
+++ webhook/views.py
@@ -2,6 +2,25 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+import hashlib
+import hmac
+import json
+import base64
+import os
+from django.conf import settings
+
+def verifySignature(receivedSignature: str, payload):
+
+    
+    WEBHOOK_SECRET = settings.typeform_secret
+
+    digest = hmac.new(WEBHOOK_SECRET.encode('utf-8'), payload, hashlib.sha256).digest()
+    
+    e = base64.b64encode(digest).decode()
+    
+    if(e == receivedSignature):
+      return True
+    return False
 
 
 @csrf_exempt
@@ -9,10 +28,26 @@ def index(request):
 
     # return HttpResponse("Hello, world. You're at the polls index.")
     if request.method == 'POST':
-            
-        request_body = json.loads(request.body)
 
-        print("Typeform webhook", request_body)
+        raw = request.body
+        request_body = json.loads(raw)
+        
+        receivedSignature = request.headers.get("typeform-signature")
+
+        if receivedSignature is None:
+            return HttpResponse(status=403, detail="Permission denied.")
+        
+        sha_name, signature = receivedSignature.split('=', 1)
+        if sha_name != 'sha256':
+            return HttpResponse(status=501, detail="Operation not supported.")
+
+        is_valid = verifySignature(signature, raw)
+        if(is_valid != True):
+            return HttpResponse(status=403, detail="Invalid signature. Permission Denied.")
+        
+        print("\nsignature Verified")
+
+        print("\nTypeform webhook", request_body)
 
         return JsonResponse({"status":True,"message":"success"})
 
